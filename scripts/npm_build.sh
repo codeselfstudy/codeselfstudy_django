ef2a1d6a6419dd3d0609a1d6fd5fe536ad937b10
J. Cohen <hostelmarketing@gmail.com>
2020-11-20 18:03:53 -0800

    Update npm scripts

--- scripts/npm_build.sh
+++ scripts/npm_build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 
+# delete the existing build files
+echo 'removing old build'
+rm -rf ./static/dist
+
+echo 'rebuilding the assets'
 # TODO: I don't think --no-source-maps is working correctly
 npx parcel build static/src/parcel-entry.js \
     --no-source-maps\
--- scripts/npm_start.sh
+++ scripts/npm_start.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 
+# delete the existing build files
+echo 'removing old build'
+rm -rf ./static/dist
+
+# build the assets and watch for changes
+echo 'rebuilding the assets'
 npx parcel static/src/parcel-entry.js \
     --hmr-port 34471 \
     --out-dir static/dist \
