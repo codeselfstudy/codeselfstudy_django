aaa60bbc64972eb07761300ad1027926fb6de22c
J. Cohen <hostelmarketing@gmail.com>
2020-11-27 00:58:22 -0800

    For now the app just needs to listen to Slack

--- apps/puzzles/urls.py
+++ apps/puzzles/urls.py
@@ -9,8 +9,5 @@ app_name = "puzzles"
 # prefixed with `api`. It's a temporary way to avoid using routes that might be
 # used for web ingerfaces later.
 urlpatterns = [
-    path("api/", views.random_puzzle, name="random_puzzle"),
-    path("api/slack/", views.slack_slash_command, name="slack_slash_command"),
-    path("api/<source>/", views.random_puzzle_by_source, name="random_puzzle_by_source"),
-    # path("api/<source>/<puzzle_id>/", views.puzzle_by_id, name="puzzle_by_id"),
+    path("slack/", views.slack_slash_command, name="slack_slash_command"),
 ]
--- apps/puzzles/views.py
+++ apps/puzzles/views.py
@@ -2,29 +2,6 @@
 from django.http import JsonResponse
 
 
-def random_puzzle(request):
-    payload = {
-        "hello": "world",
-    }
-    return JsonResponse(payload)
-
-
-def random_puzzle_by_source(request, source):
-    payload = {
-        "source": source,
-    }
-    return JsonResponse(payload)
-
-
-# TODO: I don't know if this is needed. People will submit queries or URLs but
-# probably not IDs, since they can paste the entire URL.
-# def puzzle_by_id(request, puzzle_id):
-#     payload = {
-#         "puzzle": puzzle_id,
-#     }
-#     return JsonResponse(payload)
-
-
 def slack_slash_command(request):
     payload = {
         "hello": "slack",
