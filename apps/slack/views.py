175c666894067e40684091f53d66f13afc5bc403
J. Cohen <hostelmarketing@gmail.com>
2020-11-27 01:49:45 -0800

    Migrate in the flask code to return a puzzle

--- apps/slack/views.py
+++ apps/slack/views.py
@@ -1,5 +1,11 @@
-from django.http import JsonResponse, Http404
+import logging
+
+from django.http import JsonResponse, Http404, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+
+from .signature import verify_signature
+
+log = logging.getLogger(__name__)
 
 
 @csrf_exempt
@@ -7,7 +13,31 @@ def puzzle_slash_command(request):
     if request.method != "POST":
         raise Http404("Not found")
 
+    slack_signature = request.headers.get("X-Slack-Signature")
+    slack_ts = request.headers.get("X-Slack-Request-Timestamp")
+
+    data = request.body.decode("utf-8")
+
+    if not verify_signature(slack_signature, slack_ts, data):
+        return HttpResponse('Unauthorized', status=401)
+
+    # TODO: parse the `data` and figure out what command to send back
+    # If can parse the query and find a puzzle in the database, send it back.
+    body = "the message that gets sent to Slack"
+
+    # TODO: get formatting ideas from https://api.slack.com/block-kit
     payload = {
-        "hello": "slack",
+        # TODO: if the request isn't successful, send the error message back but NOT in_channel.
+        "response_type": "in_channel",
+        "blocks": [
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": body,
+                },
+            },
+        ],
     }
+
     return JsonResponse(payload)
