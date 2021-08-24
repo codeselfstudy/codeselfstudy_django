4af5ed0753ced68a570b08735d31a148312f7bd4
J. Cohen <hostelmarketing@gmail.com>
2021-08-23 19:17:30 -0700

    Get the database wired up

--- codeselfstudy/settings.py
+++ codeselfstudy/settings.py
@@ -125,21 +125,14 @@ WSGI_APPLICATION = "codeselfstudy.wsgi.application"
 
 DATABASES = {
     "default": {
-        # "ENGINE": "django.db.backends.postgresql",
-        # "NAME": os.environ.get("DATABASE_NAME"),
-        # "USER": os.environ.get("DATABASE_USER"),
-        # "PASSWORD": os.environ.get("DATABASE_PASSWORD"),
-        # "HOST": os.environ.get("DATABASE_HOST"),
-        # "PORT": os.environ.get("DATABASE_PORT"),
+        "NAME": os.environ.get("DATABASE_NAME"),
+        "USER": os.environ.get("DATABASE_USER"),
+        "PASSWORD": os.environ.get("DATABASE_PASSWORD"),
+        "HOST": os.environ.get("DATABASE_HOST"),
+        "PORT": os.environ.get("DATABASE_PORT"),
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "codeselfstudy",
-        "USER": "postgres",
-        "PASSWORD": "postgres",
-        "HOST": "postgres",
-        "PORT": 5432,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
