9c24275608337425991552bfdd43de6548230847
Josh Cohen <backpack360@gmail.com>
2022-04-11 14:08:10 -0700

    Rearrange the static files.
    Removed Bulma and replaced it with simpledotcss

--- scripts/npm_start.sh
+++ scripts/npm_start.sh
@@ -3,12 +3,12 @@
 
 # delete the existing build files
 echo 'removing old build'
-rm -rf ./static/dist
+rm -rf ./assets/dist/*
 
 # build the assets and watch for changes
 echo 'rebuilding the assets'
-npx parcel static/src/parcel-entry.js \
+npx parcel assets/src/main.js \
     --hmr-port 34471 \
-    --dist-dir codeselfstudy/static/dist \
+    --dist-dir assets/dist \
     --public-url /static/dist/ #\
     # --out-file main
