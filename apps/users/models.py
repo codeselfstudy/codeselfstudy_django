293e973d9929555164725d98aa449e8b8bdbc358
Josh Cohen <backpack360@gmail.com>
2022-04-10 17:21:51 -0700

    Add the initial user model
    See `apps/users/models.py` for notes.

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,16 @@
-from django.db import models
+from django.contrib.auth.models import AbstractUser
+# from django.db import models
 
-# Create your models here.
+# For more information on this, see:
+# https://docs.djangoproject.com/en/4.0/topics/auth/customizing/#specifying-a-custom-user-model
+
+# To do extra things like normalize email addresses as lowercase and adding
+# extra fields, see this example:
+# https://docs.djangoproject.com/en/4.0/topics/auth/customizing/#a-full-example
+
+
+class User(AbstractUser):
+    # bio = models.TextField(blank=True, null=True)
+    # USERNAME_FIELD = "email"
+    # REQUIRED_FIELDS: List[str] = []
+    pass
