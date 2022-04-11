dc2ff6095bc57d9ac840a65c27508d09fd159370
Josh Cohen <backpack360@gmail.com>
2022-04-10 17:28:03 -0700

    More setup

--- config/settings.py
+++ config/settings.py
@@ -9,26 +9,43 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
+import sys
+from typing import List
 from pathlib import Path
+from datetime import datetime
+
+# import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+MEDIA_ROOT = BASE_DIR / "media"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [BASE_DIR / "assets"]
 
+# Allow us to keep the Django apps in `./apps/`
+sys.path.insert(0, os.path.join(BASE_DIR, "apps"))
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
+DEBUG: bool = "RENDER" not in os.environ
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-iu!0osx9efh)cv@jn44!qiy4pw20unw^evursu)$kb)qq_!lh7"
+SECRET_KEY = os.getenv("SECRET_KEY")
+if SECRET_KEY is None:
+    print("ERROR: SECRET_KEY not found")
+    sys.exit(1)
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# Cache-busting
+now = datetime.now()
+timestamp = datetime.timestamp(now)
+CACHE_BUSTER = hex(int(timestamp))[2:]
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS: List[str] = []
 
+RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
-# Application definition
+if DEBUG:
+    ALLOWED_HOSTS.append("localhost")
 
 INSTALLED_APPS = [
     "django.contrib.admin",
@@ -37,7 +54,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "users.apps.UsersConfig",
 ]
+
+if DEBUG is True:
+    INSTALLED_APPS += ["debug_toolbar"]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -49,12 +70,15 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
+if DEBUG is True:
+    MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]
+
 ROOT_URLCONF = "config.urls"
 
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -62,6 +86,8 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                # Make custom variables available to all templates
+                "config.helpers.context_processors.add_cachebuster_to_context",
             ],
         },
     },
@@ -111,13 +137,18 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = "static/"
+if not DEBUG:
+    STATICFILES_STORAGE = "whitenoise.storage.CompressedMainfestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "users.User"
+
+# TODO: caches, logs, cors?
